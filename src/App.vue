3a318c09db017a2d30139346c097d00ba37d3108
Амир Салаев <salaevamir@gmail.com>
2021-09-13 00:12:41 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <img alt="my avatar" src="./assets/avatar.png">
+  <p>Автор картинки <a href="https://twitter.com/SH0R0KEN" class="ref">@SH0R0KEN</a> </p>
+  
+  <p>Пока данный сайт Состоит из одной странички и не несет никакой информации и функционала </p>
+   <p class="">Так что вот вам кнопка, чтобы развлечься</p>
+   <button type="button" :class="classButton" @click="changeClass"
+
+    >Нажми на меня</button>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: function () {
+    return {
+      classButton: 'btn btn-primary',
+      listOfClasses: ["btn btn-primary", "btn btn-secondary", "btn btn-success", "btn btn-danger", "btn btn-warning", "btn btn-info", "btn btn-light", "btn btn-dark"]
+    };
+  },
+  methods: {
+    changeClass(){
+      // let lenght = this.listOfClasses.length;
+      let index = this.listOfClasses.map(function(e) { return e; }).indexOf(this.classButton);
+      this.classButton = this.listOfClasses[index + 1];
+    }
   }
 }
 </script>
